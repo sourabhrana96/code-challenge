d9a097571566a093d3d4a919f5f26ef9e329aecb
sourabhrana96 <sourabhrana35@gmail.com>
2020-07-19 16:45:15 +0530

    added functionality to save or update city and state using ZipCodes gem

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,16 @@ class Company < ApplicationRecord
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/,
                   message: "addresses for a Company should only be a @getmainstreet.com domain", allow_blank: true }
 
+  validates_length_of :zip_code, minimum: 5, maximum: 5, allow_blank: false, message: "is not valid!"
+
+  before_save :set_or_update_city_and_state
+
+  def set_or_update_city_and_state
+   return {} unless self.zip_code.present?
+   zip_data = ZipCodes.identify(self.zip_code)
+    if zip_data
+      self.city = zip_data[:city]
+      self.state = zip_data[:state_code]
+   end
+  end
 end
