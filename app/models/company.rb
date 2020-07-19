3cb1190072de1cc7ea428ecab8c2ecbe99e1321a
sourabhrana96 <sourabhrana35@gmail.com>
2020-07-19 15:47:55 +0530

    email validation added

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   has_rich_text :description
-
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/,
+                  message: "addresses for a Company should only be a @getmainstreet.com domain", allow_blank: true }
 
 end
